ea9d633050175192b94a1ac4a5d8f968a0c47ce1
admin <smallrobot@naver.com>
2025-06-18 12:13:38 +0900

    starcounting

--- src/star04.java
+++ src/star04.java
@@ -1,10 +1,10 @@
 public class star04 {
     public static void main(String[] args) {
-        int size = 5;  // 배열 크기
+        // 배열 크기
 
-        for (int i = 0; i < size; i++) {          // 행
-            for (int j = 0; j < size; j++) {      // 열
-                if (i == j || i + j == size - 1) {
+        for (int i = 0; i < 5; i++) {          // 행
+            for (int j = 0; j < 5; j++) {      // 열
+                if (i == j || j == 5 - 1-i) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
