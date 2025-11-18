0f011f34110ba4a4882c3448f3701a51f681d699
jojo <smallrobot@naver.com>
2025-11-18 17:59:48 +0900

    push

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("1.");
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <=5; j++) {
-                System.out.print("*");
+                System.out.print("*$$");
             }
             System.out.println();
         }
